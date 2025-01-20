22eaa88bb739a7cdc01399c36bb8835fe78568fe
Mauricio Eduardo <mauricioeduardokoop@gmail.com>
2025-01-20 16:03:03 -0300

    Inicia a chamada e exibição da API currentWeatherApp.

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -5,36 +5,53 @@ import { theme } from "./styles/theme";
 import CurrentWeather from "./components/CurrentWeather";
 import WeatherForecast from "./components/WeatherForecast";
 
-
-
 const App = () => {
 	const [currentWeatherData, setCurrentWeatherData] = useState(null);
 	const [forecastData, setForecastData] = useState([]);
 	const [loading, setLoading] = useState(true);
 	
 	// Aqui serão definidas as chaves da API
-	const API_KEY = 0e503de2b1939e5bb863b9f63a362e42;
-	const lat = -15.7975;
-	const lon = -47.8919;
+	const lat = -26.7745146261508;
+	const lon = -48.64422810098111;
+	const API_CONECTION = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=0e503de2b1939e5bb863b9f63a362e42`;
 
 	useEffect(() => {
 		const fetchWeatherData = async () => {
 			try {
-				const currentWeatherResponse = await fetch(
-					`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
-				);
-				const currentWeatherJson = currentWeatherResponse.json();
+				const currentWeatherResponse = await fetch(API_CONECTION);
+				const currentWeatherJson = await currentWeatherResponse.json();
+
 				setCurrentWeatherData({
 					location: currentWeatherJson.name,
-					temperature: currentWeatherJson.main.temp
+					temperature: (currentWeatherJson.main.temp - 273.15).toFixed(2)
 				});
 
 				// Previsão do tempo 5 dias
-				
+			}catch (error) {
+				console.error("Erro ao buscar os dados:", error);
+			} finally {
+				setLoading(false);
 			}
 		}
-	})
+
+		fetchWeatherData();
+	}, []); // O array vazio garante que o useEffect rode apenas uma vez.
 	
+	if(loading){
+		<div>Carregando dados...</div>
+	}
+
+	return(
+		<ThemeProvider theme={theme}>
+			<GlobalStyles/>
+			{currentWeatherData && (
+				<CurrentWeather
+					location={currentWeatherData.location} 
+					temperature={currentWeatherData.temperature + " C°"}
+				/>
+			)}
+		</ThemeProvider>
+	)
 }
 
 export default App;
